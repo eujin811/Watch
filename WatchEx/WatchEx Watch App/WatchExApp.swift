a1e7eceb23a8a073a738d8d3950e7bcb15ac4b35
eujin811 <eujin8@gmail.com>
2023-01-16 21:42:03 +0900

    :tada: watch ex project

---
+++ WatchEx/WatchEx Watch App/WatchExApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchExApp.swift
+//  WatchEx Watch App
+//
+//  Created by YoujinMac on 2023/01/16.
+//
+
+import SwiftUI
+
+@main
+struct WatchEx_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WatchEx/WatchEx/WatchExApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchExApp.swift
+//  WatchEx
+//
+//  Created by YoujinMac on 2023/01/16.
+//
+
+import SwiftUI
+
+@main
+struct WatchExApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
