8b4bf360295f60c15f070adea3537616b6d08c6f
eujin811 <eujin8@gmail.com>
2023-02-06 22:59:38 +0900

    :construction: 충전기 list 더미

--- WatchEx/WatchEx Watch App/Charger/View/ChargerListView.swift
+++ WatchEx/WatchEx Watch App/Charger/View/ChargerListView.swift
@@ -32,7 +32,7 @@ final class ChargerListStore: ObservableObject {
             }
         }
     }
-
+    
 }
 
 struct ChargerListView: View {
@@ -43,15 +43,55 @@ struct ChargerListView: View {
     
     var body: some View {
         List(store.chargerList, id: \.id) { item in
-            Button("item \(item.stationName)") {
-                showChargerDetailView.toggle()
-                store.showItem = item
+            VStack {
+                HStack {
+                    Image(systemName: "photo.circle")
+                        .frame(width: 12, height: 12)
+                    Text(item.operation)    // fontsize 8
+                        .font(.system(size: 8, weight: .semibold))
+                    Spacer()
+                    Text("xx km")
+                }
+                .frame(height: 14)
+                .padding(.top, 10)
+                
+                Text(item.stationName)  // fontSize 12
+                    .font(.system(size: 12, weight: .bold))
+                    .frame(height: 16)
+                
+                HStack {
+                    Text("급속 어쩌고~ 완속 어쩌고")
+                        .font(.system(size: 10, weight: .semibold))
+                }
+                
+                ScrollView(.horizontal) {
+                    LazyHStack {
+                        CellView()
+                            .frame(width: 91, height: 35)
+                            .background(.gray)
+                        
+                        CellView()
+                            .frame(width: 91, height: 35)
+                            .background(.gray)
+                        
+                        CellView()
+                            .frame(width: 91, height: 35)
+                            .background(.gray)
+                        
+                    }
+                }
             }
-            .sheet(isPresented: $showChargerDetailView) {
-                ChargerDetailView()
-                    .environmentObject(ChargerDetailStore(myLocation: store.myLocation, item: store.showItem))
-            }
-    
+            .frame(height: 119)
+            .cornerRadius(3)
+            
+        }
+        .listStyle(CarouselListStyle())
+        .padding(.leading, 8)
+        .padding(.trailing, 8)
+    }
+}
+
+
 fileprivate struct CellView: View {
     
     var body: some View {
