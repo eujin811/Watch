4adb0cc680a9781aa142bb01b9e2172f11220c80
eujin811 <eujin8@gmail.com>
2023-02-06 21:36:38 +0900

    :construction: Location

---
+++ WatchEx/WatchEx Watch App/Charger/Model/Location.swift
@@ -0,0 +1,13 @@
+//
+//  Location.swift
+//  WatchEx Watch App
+//
+//  Created by YoujinMac on 2023/02/06.
+//
+
+import Foundation
+
+struct Location {
+    let lat: Double
+    let lon: Double
+}
