c6519b20f925e513fe9c7e7eb67219d42897126d
eujin811 <eujin8@gmail.com>
2023-02-13 20:05:45 +0900

    :construction: WCError

---
+++ WatchEx/WatchEx/Common/WCError.swift
@@ -0,0 +1,14 @@
+//
+//  WCError.swift
+//  WatchEx
+//
+//  Created by YoujinMac on 2023/02/13.
+//
+
+import Foundation
+
+enum WCError: Error {
+    case unknownData
+    case unknownType
+    case failRespose
+}
