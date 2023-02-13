95aa7fea7437b7ac23fac755bfe1e2138b2d2096
eujin811 <eujin8@gmail.com>
2023-02-13 20:30:00 +0900

    :construction: Protect file

---
+++ WatchEx/WatchEx Watch App/Protect/ProtectConstants.swift
@@ -0,0 +1,15 @@
+//
+//  ProtectConstants.swift
+//  WatchEx
+//
+//  Created by YoujinMac on 2023/02/13.
+//
+
+import Foundation
+
+enum ProtectConstants {
+    
+    enum APIURL: String {
+        
+    }
+}
