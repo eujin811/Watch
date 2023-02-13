445a8a39c7055b7046c0bb35aa95ac9034da1a82
eujin811 <eujin8@gmail.com>
2023-02-13 22:07:10 +0900

    :construction: WCError 추가

--- WatchEx/WatchEx/Common/WCError.swift
+++ WatchEx/WatchEx/Common/WCError.swift
@@ -11,4 +11,29 @@ enum WCError: Error {
     case unknownData
     case unknownType
     case failRespose
+    case dataTask
+    case dataDecode
+    case decode
+    
+    var message: String {
+        switch self {
+        case .unknownData:
+            return "데이터를 미일치"
+            
+        case .unknownType:
+            return "타입 미일치"
+            
+        case .failRespose:
+            return "reponse 실패"
+            
+        case .dataTask:
+            return "task 실패"
+            
+        case .dataDecode:
+            return "데이터 decode 실패"
+            
+        case .decode:
+            return "decode error"
+        }
+    }
 }
