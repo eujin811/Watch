52c7b99b97c590062eddf57b858c1c0b00fd45c0
eujin811 <eujin8@gmail.com>
2023-02-13 20:32:42 +0900

    :construction: Constants

---
+++ WatchEx/WatchEx Watch App/Costants/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  WatchEx Watch App
+//
+//  Created by YoujinMac on 2023/02/13.
+//
+
+import Foundation
+
+enum Constants {
+    static let apiURL = ProtectConstants.APIURL.self
+}
