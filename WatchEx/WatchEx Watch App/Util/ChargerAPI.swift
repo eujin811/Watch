6f17eff8ebf4143d16acd2b6fdb5097e86063476
eujin811 <eujin8@gmail.com>
2023-02-19 21:13:05 +0900

    :construction: ChargerAPI AF 버전

--- WatchEx/WatchEx Watch App/Util/ChargerAPI.swift
+++ WatchEx/WatchEx Watch App/Util/ChargerAPI.swift
@@ -7,25 +7,56 @@
 
 import Foundation
 import Combine
+import Then
 
 import Alamofire
 
 protocol ChargerAPIProtocol {
-//    func getStationList(lat: Float, lon: Float) -> Result<ChargerListModel, AFError>
     func getStationList(
         lat: Double,
         lon: Double,
         completion: @escaping (Result<ChargerListModel, AFError>) -> Void)
+    // alamofire
+    func getStationListAF(lat: Double, lon: Double) -> AnyPublisher<ChargerListModel?, Never>?
 
 }
 
 final class ChargerAPI: ChargerAPIProtocol {
     func getStationList(lat: Double, lon: Double, completion: @escaping (Result<ChargerListModel, AFError>) -> Void) {
+    // Alamofire 버전
+    func getStationListAF(lat: Double, lon: Double) -> AnyPublisher<ChargerListModel?, Never>? {
+        
         let reqParam: Parameters = [
             "lat": "\(lat)",
             "lon": "\(lon)"
         ]
 
+        return Future<ChargerListModel?, Never> { promise in
+            AF.request("https://dev.soft-berry.co.kr/test/test/station_list",
+                       method: .get,
+                       parameters: reqParam,
+                       encoding: URLEncoding.default)
+            .responseDecodable(of: ChargerListModel.self) { response in
+
+                switch response.result {
+                case .success(let result):
+                    promise(.success(result))
+                    print("성공 \(result)")
+
+                case .failure(let error):
+                    promise(.success(nil))
+                }
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    func getStationList(lat: Double, lon: Double, completion: @escaping (Result<ChargerListModel, AFError>) -> Void) {
+        let reqParam: Parameters = [
+            "lat": "\(lat)",
+            "lon": "\(lon)"
+        ]
+        
         AF.request("https://dev.soft-berry.co.kr/test/test/station_list",
                    method: .get,
                    parameters: reqParam,
