6ff2918fc23f2a98ce43cf99193f70a388686d59
eujin811 <eujin8@gmail.com>
2023-03-05 22:02:48 +0900

    :construction: LocationAuthorityView 파일생성

---
+++ WatchEx/WatchEx Watch App/CustomView/LocationAuthorityView.swift
@@ -0,0 +1,22 @@
+//
+//  LocationAuthority.swift
+//  WatchEx Watch App
+//
+//  Created by YoujinMac on 2023/03/05.
+//
+
+import SwiftUI
+
+struct LocationAuthorityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .foregroundColor(Color(.gray600))
+            
+    }
+}
+
+struct LocationAuthority_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationAuthorityView()
+    }
+}
