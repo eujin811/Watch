d17e1831b05bbdebd39cf377be1c8dfe43e13894
eujin811 <eujin8@gmail.com>
2023-03-05 22:01:23 +0900

    :construction: Colors set / gray, green

--- WatchEx/GeneralComponent/DesignComposition/Colors.swift
+++ WatchEx/GeneralComponent/DesignComposition/Colors.swift
@@ -7,6 +7,26 @@
 
 import Foundation
 
-enum Colors {
+enum Colors: Int {
+    // MARK: Gray
+    case gray100 = 0xF5F6F7
+    case gray200 = 0xDEE1E4
+    case gray300 = 0xC5C8CB
+    case gray400 = 0xACAFB2
+    case gray500 = 0x939699
+    case gray600 = 0x7A7D80
+    case gray700 = 0x616467
+    case gray800 = 0x484B4E
+    case gray900 = 0x2F3235
     
+    // MARK: Green
+    case green100 = 0xD7F9E8
+    case green200 = 0xA6F2CE
+    case green300 = 0x61E8A7
+    case green400 = 0x3CD697
+    case green500 = 0x18CC8B
+    case green600 = 0x10895E
+    case green700 = 0x0C6E50
+    case green800 = 0x074F3B
+    case green900 = 0x02332A
 }
