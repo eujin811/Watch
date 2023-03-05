ef6f4071e5b8b99f08afcfef024406d5004710a9
eujin811 <eujin8@gmail.com>
2023-03-05 23:29:47 +0900

    :construction: Font extension

---
+++ WatchEx/GeneralComponent/Extension/Font+.swift
@@ -0,0 +1,28 @@
+//
+//  Font+.swift
+//  WatchEx Watch App
+//
+//  Created by YoujinMac on 2023/03/05.
+//
+
+import SwiftUI
+
+extension Font {
+//    init(weight: FontType, size: CGFloat) {
+////        self.init(.system(size: size, weight: weight.rawValue))
+////        self.init(.system(size: size, weight: .thin))
+//        
+//    }
+//    
+//    enum FontType: Font.Weight {
+//        case 100 = .ultraLight
+//        case 200 = .thin
+//        case 300 = .light
+//        case 400 = .regular
+//        case 500 = .medium
+//        case 600 = .semibold
+//        case 700 = .bold
+//        case 800 = .heavy
+//        case 900 = .black
+//    }
+}
